8312d5c73489778ddc4867cc9d531fd464079c56
mspzh <32201793+mspzh@users.noreply.github.com>
2018-01-24 18:41:21 +0000

    Update tokeniser.py

--- tokeniser.py
+++ tokeniser.py
@@ -1,35 +1,40 @@
-import codecs
-import sys
 import re
 
-igbo = codecs.open('wiki.txt', 'r', 'utf-8')
-igbo_text = igbo.read()
-sent = []
-sent_words = []
-counter = 0 #This is the counter for IDs
-counter_wordiness = 0 #This is the counter for words inside the sentences
-tokenis = codecs.open('./tokenized.txt', 'w', 'utf-8-sig')
+PUNCT_ARR = '".!?:,()'
 
-igbo_text = re.sub(u'([.!?:])', u'\\1ə', igbo_text)
-sent = igbo_text.split(u'ə')
+def inf_to_sentences(text):
+    text = text.replace("\n", "")
+    text = re.sub('([.!?:])', '\\1ə', text)
+    sentences = text.split("ə")
+    return sentences
 
-for j in sent:
-    j = j.strip()
-    if j is '':
-        continue
+
+def sentences_processing(sentence, counter):
+    words_to_write = []
+    sentence = sentence.strip()
+    if sentence:
+        j = 0
+        sent_to_write = '#sent_id = ' + str(counter+1) + '\n#text = ' + sentence + '\n'
+        for word in sentence.split(" "):
+            new_word = word.strip(PUNCT_ARR)
+            if word[0] in PUNCT_ARR: #I presume there punct sign can't arise somewhere else than in the end of the word (but for quotes and brackets) and that there is only one there
+                j+=1
+                words_to_write.append(str(j) + '\t' + word[0] + '\t_'*8 + '\n')
+            j+=1
+            words_to_write.append(str(j) + '\t' + new_word + '\t_'*8 + '\n')
+            if word[-1] in PUNCT_ARR:
+                j+=1
+                words_to_write.append(str(j) + '\t' + word[-1] + '\t_'*8 + '\n')
     else:
-        counter += 1
-        tokenis.write(u'#sent_id = ' + str(counter) + u'\n#text = ' + j + u'\n')
-        j = re.sub('([\(\)"?!.])', ' \\1', j)
-        sent_words = j.split(' ')
-        for k in sent_words:
-            if k is '':
-                continue
-            else:
-                k = k.strip()
-                counter_wordiness += 1
-                tokenis.write(str(counter_wordiness) + u'\t' + k + u'\t_'*8 + u'\n')
-                print(str(counter_wordiness) + u'\t' + k + u'\t_'*8 + u'\n')
-                if '[?!.]' in k:
-                    continue
-        counter_wordiness = 0
+        return '', ['']
+    return sent_to_write, words_to_write
+
+if __name__ == '__main__':
+    with open("wiki.txt") as infile:
+        text = infile.read()
+        igbo_text = inf_to_sentences(text)
+        with open('tokenized_DK.txt', 'w') as outfile:
+            for i, sentence in enumerate(igbo_text):
+                sent, words = sentences_processing(sentence, i)
+                outfile.write(sent)
+                outfile.write(''.join(words))
