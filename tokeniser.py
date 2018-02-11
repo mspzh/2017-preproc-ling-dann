a4251c8eeb01aaf25114b563f9ae4d3fde32c257
mspzh <32201793+mspzh@users.noreply.github.com>
2018-02-11 20:51:53 +0000

    Update tokeniser.py

--- tokeniser.py
+++ tokeniser.py
@@ -1,4 +1,4 @@
-import re
+import re, sys
 
 PUNCT_ARR = '".!?:,()'
 
@@ -30,10 +30,10 @@ def sentences_processing(sentence, counter):
     return sent_to_write, words_to_write
 
 if __name__ == '__main__':
-    with open("wiki.txt") as infile:
+    with open(sys.stdin) as infile:
         text = infile.read()
         igbo_text = inf_to_sentences(text)
-        with open('tokenized.txt', 'w') as outfile:
+        with open(sys.stdout) as outfile:
             for i, sentence in enumerate(igbo_text):
                 sent, words = sentences_processing(sentence, i)
                 outfile.write(sent)
